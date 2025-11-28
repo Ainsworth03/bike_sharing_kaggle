3aab94fd0322235b5430072daa5daa8c84ccdbe2
Ainsworth03 <143927821+Ainsworth03@users.noreply.github.com>
2025-11-28 16:37:49 +0700

    Update Error Filepath on Streamlit
    Change line 12-13 directory

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,8 +9,8 @@ cwd = os.getcwd()
 
 sns.set(style='dark')
 
-rev_day_dir = os.path.join(os.getcwd(), 'rev_day.csv')
-rev_hour_dir = os.path.join(os.getcwd(), 'rev_hour.csv')
+rev_day_dir = os.path.join('rev_day.csv')
+rev_hour_dir = os.path.join('rev_hour.csv')
 
 day_df = pd.read_csv(rev_day_dir)
 hour_df = pd.read_csv(rev_hour_dir)
@@ -596,6 +596,7 @@ else:
 
     main_view_CustomDate()
     show_additional()
+
 
 
 
